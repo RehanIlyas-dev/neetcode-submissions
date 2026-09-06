7dd1611fcec85e5c5670fcb5552dc996af17633d
Rehan Ilyas <rehanilyas4726@gmail.com>
2026-09-06 18:34:53 +0500

    Delete Data Structures & Algorithms/two-integer-sum/submission-1.py

--- Data Structures & Algorithms/two-integer-sum/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        HashMap = {}
-        for idx, num in enumerate(nums):
-            compliment = target - num # Optimal Approach [Y=Target-X] in Brute  Force it would be X+Y=Target
-            if compliment in HashMap:
-                return [HashMap[compliment],idx]
-            HashMap[num] = idx
-        return []
